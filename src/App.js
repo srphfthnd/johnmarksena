bc429d9bcfc9ba5419d1d1f2bfe3dc540c37c1b5
shnscht <yellowpad0002@gmail.com>
2022-07-16 03:07:37 +0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Navbar from '../src/components/navbar/navbar.tsx'
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
