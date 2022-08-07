d0d91d5cdd56821b3e9a965458854dd74bfc0e87
shnscht <yellowpad0002@gmail.com>
2022-08-07 17:45:28 +0800

    mend
    feature/1 fixed merge conflicts

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,10 +1,15 @@
-import React from "react";
-import './navbar.scss';
+import React from 'react';
+import './Navbar.scss'
 
 function Navbar() {
     return (
-        <div>
-            <h1>Check</h1>
+        <div className="nav">
+            <ul className="nav-menu">
+                <li><a href="#home">Home</a></li>
+                <li><a href="#news">News</a></li>
+                <li><a href="#contact">Contact</a></li>
+                <li><a href="#about">About</a></li>
+            </ul>
         </div>
     );
 }
